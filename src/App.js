e1d6ee21df8bb3bcbd8034daa307f7c7d76453ad
Albert Ong <albert_ong_us@yahoo.com>
2023-08-17 17:39:01 -0700

    Progress on webpage conversion

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-// import site_logo from "./public/site_logo.png";
+import site_logo from "./assets/site_logo.png";
+import location_icon from "./assets/location_icon.png";
+import github_icon from "./assets/github_icon.png";
+import email_icon from "./assets/email_icon.png";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -6,19 +9,22 @@ import './App.css';
 function App() {
 
   document.title = 'Albert Ong';
+
   return (
-    <div>
+    <div 
+      style={{
+        backgroundImage: "url(${background})",
+      }}
+    >
+
       <nav class="navbar navbar-expand-sm navbar-dark">
         <div class="container-fluid">
           <div class="collapse navbar-collapse">
-
             <a class="navbar-brand">
-                <img class="site-logo" src="site_logo.png" alt="Logo"/>
+                <img src={site_logo} alt="Logo" height="80px"/>
             </a>
-
-            <ul class="navbar-nav">
-
-              <li class="nav-item">
+            <ul class="navbar-nav mt-5">
+              <li class="nav-item ml-5">
                 <a class="nav-link" href="about.html">ABOUT</a>
               </li>
               <li class="nav-item">
@@ -32,10 +38,29 @@ function App() {
         </div>
       </nav> 
 
-      <h1 class="pt-5">Hi, I'm Albert!</h1>
-      <p class="subtitle">I'm a software engineer and hobbiest graphic designer.</p>
-      <p>San Francisco Bay Area</p>
-      <h2 class="text-center">PROJECTS</h2>
+      <div class="heading">
+        <h1 class="pt-5">Hi, I'm Albert!</h1>
+        <p class="subtitle mt-2">I'm a software engineer and hobbiest graphic designer.</p>
+        <div class="mt-15">
+          <img class="mb-1" src={location_icon} alt="location" height="20px"/><p class="location-text">San Francisco Bay Area</p>
+        </div>
+      </div>
+      
+      {/* <h2 class="text-center mb-5">PROJECTS</h2> */}
+
+      <nav class = "navbar navbar-default navbar-bottom">
+        <ul class="navbar-nav list-group-horizontal mt-5">
+          <li>
+            <a href="https://github.com/Albert-C-Ong" target="_blank"><img class="navbar-bottom-icon navbar-bottom-icon-link" src={github_icon} alt="GitHub icon"/>Albert-C-Ong</a>
+          </li>
+          <li>
+            <a href="mailto:albert_ong_us@yahoo.com"><img class="navbar-bottom-icon navbar-bottom-icon-link" src={email_icon} alt="email icon"/>albert_ong_us@yahoo.com</a>
+          </li> 
+          <li>
+            <img class="navbar-bottom-icon" src={location_icon} alt="location icon"/>San Jose, CA 95132
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
