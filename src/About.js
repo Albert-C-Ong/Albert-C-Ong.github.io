69b7937459cfe803a50f4474ddfb8464e8c79aae
Albert Ong <albert_ong_us@yahoo.com>
2023-08-31 16:50:09 -0700

    Progress on project images

--- src/About.js
+++ src/About.js
@@ -21,22 +21,20 @@ function About() {
 
       <div class="modal-body row m-5">
         <div class="col-md-6">
-          <div class="">
             <h1 class="">About</h1>
             
-            <div class="fs-5" style={{hover: "red"}}>
+            <div class="fs-5">
               Hi, I'm a software engineering major from San Jose State University and a 
               hobbiest graphic designer with experience in web design and programming.
 
               I have most recently worked at Perspecta as a software engineer intern. I'm passionate about learning and 
               my personal motto is that you're never too old to learn something new. Other interests of mine include reading, gaming, and memeing. 
 
-              <p class="mt-5">For inquiries, contact <a class="cornflowerblue" href="mailto:albert_ong_us@yahoo.com">albert_ong_us@yahoo.com</a></p>
+              <p class="mt-4">For inquiries, contact <a class="cornflowerblue" href="mailto:albert_ong_us@yahoo.com">albert_ong_us@yahoo.com</a></p>
             </div>
-          </div>
         </div>
         <div class="col-md-6 text-center">
-          <img src={about_icons} alt="about icons" height="500px"/>
+          <img src={about_icons} alt="about icons" height="60%"/>
         </div>
       </div>
 
--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,10 @@ Created: 2023.08.18
 
 import React from "react";
 import location_icon from "./assets/location_icon.png";
+import cantonese_vocabulary_table_1 from "./assets/project images/cantonese_vocabulary_table_1.png";
+import cosaic_1 from "./assets/project images/cosaic_1.png";
+import lyfestyle_1 from "./assets/project images/lyfestyle_1.png";
+import virisol_1 from "./assets/project images/virisol_1.png";
 import Navbar from './Navbar';
 import BottomNavbar from './BottomNavbar';
 
@@ -22,12 +26,29 @@ const Home = () => {
         <h1 class="pt-5">Hi, I'm Albert!</h1>
         <p class="fs-2 mt-2">I'm a software engineer and hobbiest graphic designer.</p>
         <div class="mt-15">
-          <img class="mb-1" src={location_icon} alt="location" height="20px"/><p class="location-text">San Francisco Bay Area</p>
+          <img src={location_icon} alt="location" height="20px"/><p class="location-text">San Francisco Bay Area</p>
         </div>
       </div>
       
-      {/* TODO */}
-      {/* <h2 class="text-center">PROJECTS</h2> */}
+      <h2 class="text-center mb-3">PROJECTS</h2>
+      <div style={{width: "600px"}} class="container text-center mb-5">
+        <div class="row">
+          <div class="col-md-6">
+            <img class="project-image rounded" src={lyfestyle_1} alt="lyfestyle"/>
+          </div>
+          <div class="col-md-6">
+            <img class="project-image rounded" src={cosaic_1} alt="cosaic"/>
+          </div>
+        </div>
+        <div class="row mt-4">
+          <div class="col-md-6">
+            <img class="project-image rounded" src={cantonese_vocabulary_table_1} alt="cantonese vocabulary table"/>
+          </div>
+          <div class="col-md-6">
+            <img class="project-image rounded" src={virisol_1} alt="virisol"/>
+          </div>
+        </div>
+      </div>
       
       <BottomNavbar/>
     </div>
