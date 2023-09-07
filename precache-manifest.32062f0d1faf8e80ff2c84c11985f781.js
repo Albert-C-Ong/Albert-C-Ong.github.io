4aa35ca8fcc86cd74b2aaca1eea61944a2a5dddb
Albert Ong <albert_ong_us@yahoo.com>
2023-09-06 17:06:27 -0700

    Updates

---
+++ precache-manifest.32062f0d1faf8e80ff2c84c11985f781.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "a657c09978a0892fec69",
+    "url": "/static/css/main.b2c566eb.chunk.css"
+  },
+  {
+    "revision": "a657c09978a0892fec69",
+    "url": "/static/js/main.4b43fbcc.chunk.js"
+  },
+  {
+    "revision": "753201d7370095a75750",
+    "url": "/static/js/runtime~main.03d47f1d.js"
+  },
+  {
+    "revision": "d43c087deff2efcaa8e6",
+    "url": "/static/css/2.d49905a6.chunk.css"
+  },
+  {
+    "revision": "d43c087deff2efcaa8e6",
+    "url": "/static/js/2.d20b9a81.chunk.js"
+  },
+  {
+    "revision": "23c09f75324934ab93d3",
+    "url": "/static/js/3.e660b796.chunk.js"
+  },
+  {
+    "revision": "c33066cc74783fda7ad1ca1ec644cea6",
+    "url": "/static/media/cantonese_vocabulary_table_1.c33066cc.png"
+  },
+  {
+    "revision": "a960b69eb5e90390114a239eb9ae837f",
+    "url": "/static/media/cosaic_1.a960b69e.png"
+  },
+  {
+    "revision": "42c58d6cb507dd0169d0e1d1812983ba",
+    "url": "/static/media/lyfestyle_1.42c58d6c.png"
+  },
+  {
+    "revision": "554dc16d70385eae5196c920c2ca6926",
+    "url": "/static/media/virisol_1.554dc16d.png"
+  },
+  {
+    "revision": "f84df715e0f0684f12f494befae773cd",
+    "url": "/static/media/profile_icon.f84df715.png"
+  },
+  {
+    "revision": "d0b932ec8001061d1e336a00e4727ca9",
+    "url": "/static/media/cantonese_vocabulary_table_2.d0b932ec.png"
+  },
+  {
+    "revision": "0d8a2b5207f688382512808203e8e0e4",
+    "url": "/static/media/cantonese_vocabulary_table_3.0d8a2b52.png"
+  },
+  {
+    "revision": "bf95c57c7c39b1952e14dce9621bec09",
+    "url": "/static/media/cosaic_2.bf95c57c.png"
+  },
+  {
+    "revision": "691d0045d4f7cbf51cab59e18085b44a",
+    "url": "/static/media/cosaic_3.691d0045.png"
+  },
+  {
+    "revision": "14494012189f4c4c609f475d0951c66c",
+    "url": "/static/media/lyfestyle_2.14494012.png"
+  },
+  {
+    "revision": "9f19df44f7947370c4b6c002d6c00f8d",
+    "url": "/static/media/lyfestyle_3.9f19df44.png"
+  },
+  {
+    "revision": "47a7134b589db94f3f0b8dc4f2ecc351",
+    "url": "/static/media/virisol_2.47a7134b.png"
+  },
+  {
+    "revision": "f4723f242b3ba6127948724b02594f8b",
+    "url": "/static/media/virisol_3.f4723f24.png"
+  },
+  {
+    "revision": "31f6f783096915d2cebdda07a7bb04b3",
+    "url": "/static/media/virisol_4.31f6f783.png"
+  },
+  {
+    "revision": "54557a1e07cc1884a1bedb6821cfd4b3",
+    "url": "/static/media/about.54557a1e.png"
+  },
+  {
+    "revision": "123f6dfd9d69e65cd45853a435e50f9e",
+    "url": "/static/media/background.123f6dfd.png"
+  },
+  {
+    "revision": "b54724f54d4dd3f6796e3c4cc422f998",
+    "url": "/static/media/GothamBook.b54724f5.ttf"
+  },
+  {
+    "revision": "8566e2336952927984495865df90963c",
+    "url": "/static/media/GothamLight.8566e233.ttf"
+  },
+  {
+    "revision": "77171d8f5b5283f9d47a3434704bf944",
+    "url": "/static/media/GothamMedium.77171d8f.ttf"
+  },
+  {
+    "revision": "db33e70bc9dee9fa9ae9737ad83d77ba",
+    "url": "/static/media/GothamBold.db33e70b.ttf"
+  },
+  {
+    "revision": "96cd798b74785fec7a0da059adaf43cd",
+    "url": "/index.html"
+  }
+];
